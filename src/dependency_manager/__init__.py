7a42e8dbd61f9a9a2d0b771b8962bbfd19d57693
clavedeluna <danalitovsky+git@gmail.com>
2023-07-20 07:40:55 -0300

    impl dependency manager with pkg_resources

--- src/dependency_manager/__init__.py
+++ src/dependency_manager/__init__.py
@@ -1,1 +1,1 @@
-from .manager import Dependency, DependencyManagerAbstract
+from .manager import DependencyManagerAbstract
--- src/dependency_manager/manager.py
+++ src/dependency_manager/manager.py
@@ -1,30 +1,49 @@
 from abc import ABC, abstractmethod
-from collections import namedtuple
+from pathlib import Path
+import pkg_resources
 from .singleton import Singleton
 
-Dependency = namedtuple("Dependency", ["name", "version"])
 
 class DependencyManagerAbstract(Singleton, ABC):
     def init(self):
         """One-time class initialization."""
         self.parent_directory = self.get_parent_dir()
+        self.dependency_file = self._infer_dependency_files()
         self.dependencies = self._infer_dependencies()
 
     @abstractmethod
-    def get_parent_dir(self) -> str:
+    def get_parent_dir(self) -> Path:
         """Must define which directory to use to start looking for the
-        dependency file. Can return a string or something more complex.
+        dependency file. Return a `Path` object, for example, `Path(os.getcwd())`
         """
         pass
 
-    def add(self, dependencies: list[Dependency]):
-        pass
-    def remove(self, dependencies: list[Dependency]):
+    def add(self, dependencies: list[str]):
+        """add any number of dependencies to the end of list of dependencies."""
+        for dep_str in dependencies:
+            try:
+                dep = pkg_resources.Requirement(dep_str)
+            except pkg_resources.extern.packaging.requirements.InvalidRequirement:
+                continue
+            self.dependencies.append(dep)
+
+    def remove(self, dependencies: list[str]):
+        """remove any number of dependencies from the list of dependencies"""
         pass
     def write(self):
-        pass
+        dependencies = [str(req) for req in self.dependencies]
+        with open(self.dependency_file, "w", encoding="utf-8") as f:
+            f.writelines("\n".join(dependencies))
 
-    def _infer_dependencies(self) -> list:
-        # breakpoint()
-        self.parent_directory
-        # glob for very first requirements.txt file found
+    def _infer_dependency_files(self) -> Path:
+        # For now for simplicity only return the first file
+        return next(Path(self.parent_directory).rglob("requirements.txt"))
+
+    def _infer_dependencies(self) -> list[pkg_resources.Requirement]:
+        """
+        Extract list of dependencies from requirements.txt file.
+        Same order of requirements is maintained, no alphabetical sorting is done.
+        """
+        with open(self.dependency_file, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+        return list(pkg_resources.parse_requirements(lines))
